f955bad9d77f12a9c188a860e493c003cbf0457e
Yeonho Jang <siabard@gmail.com>
2022-01-09 21:46:44 +0900

    SDL 초기버전 만들기

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let window = video_subsystem
+        .window("SDL JRPG", 640, 480)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().accelerated().build().unwrap();
+    let mut running = true;
+    while running {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => {
+                    running = false;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    running = false;
+                }
+                _ => {}
+            }
+        }
+        canvas.clear();
+        canvas.present();
+    }
 }
